9d17083e5c26b654604b222b45271edc4944c127
yowcow <yowcow@cpan.org>
2019-12-13 11:12:14 +0900

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+ezserve
+=======
+
+Easily serve static files in a directory
+
+HOW TO USE
+----------
+
+    ezserve [-addr <address>] [-root <directory>]
+
+`-addr`: Address to bind (default to `:10080`)
+`-root`: Directory to serve (relative to current, default to `.`)
+
+INSTALL
+-------
+
+    go get -u -v github.com/yowcow/ezserve
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr string
+var root string
+
+func init() {
+	flag.StringVar(&addr, "addr", ":10080", "address to bind")
+	flag.StringVar(&root, "root", ".", "root directory")
+	flag.Parse()
+}
+
+func main() {
+	log.Println("serving static files under", root, "at address", addr)
+
+	fs := http.FileServer(http.Dir(root))
+
+	log.Fatalln(http.ListenAndServe(addr, fs))
+}
